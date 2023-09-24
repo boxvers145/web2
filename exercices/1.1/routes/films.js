dbcd59cbfcebec2c91698c9977cadb6bc6060c54
boxvers145 <62158594+boxvers145@users.noreply.github.com>
2023-09-24 12:49:56 +0200

    1.1 API : read.

---
+++ exercices/1.1/routes/films.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+const MENU = [
+  {
+    id: 1,
+    title: "Interstellar",
+    duration: 149,
+    budget: 165,
+    link: "https://www.imdb.com/title/tt0816692/",
+  },
+  {
+    id: 2,
+    title: "Memento",
+    duration: 91,
+    budget: 9,
+    link: "https://www.imdb.com/title/tt0209144/",
+  },
+  {
+    id: 3,
+    title: "Ponyo",
+    duration: 84,
+    budget: 22,
+    link: "https://www.imdb.com/title/tt0876563/",
+  },
+];
+
+// Read all the pizzas from the menu
+router.get('/', (req, res, next) => {
+  console.log('GET /films');
+  res.json(MENU);
+});
+
+module.exports = router;
