6636d8f87e6c25c68178b8ea5a501dbd2f319851
boxvers145 <62158594+boxvers145@users.noreply.github.com>
2023-09-29 14:57:20 +0200

    1.3 : API : create & REST client.

--- exercices/1.2/routes/films.js
+++ exercices/1.2/routes/films.js
@@ -26,9 +26,59 @@ const MENU = [
   },
 ];
 
-// Read all the pizzas from the menu
+// Read all the films from the menu
 router.get('/', (req, res, next) => {
-    res.json(MENU);
-  });
+  const orderByDuration =
+  req?.query?.order?.includes('duration')
+      ? req.query.order
+      : undefined;
+  let orderedMenu;
+  console.log(`order by ${orderByDuration ?? 'not requested'}`);
+  if (orderByDuration)
+    orderedMenu = [...MENU].sort((a, b) => a.duration == b.duration);
+  if (orderByDuration === '-duration') orderedMenu = orderedMenu.reverse();
+
+
+  console.log('GET /films');
+  res.json(orderedMenu ?? MENU);
+});
+
+router.get('/:id', (req, res) => {
+  console.log(`GET /films/${req.params.id}`);
+
+  const indexOfFilmFound = MENU.findIndex((film) => film.id == req.params.id);
+
+  if (indexOfFilmFound < 0) return res.sendStatus(404);
+
+  res.json(MENU[indexOfFilmFound]);
+});
+
+// Create a pizza to be added to the menu.
+router.post('/', (req, res) => {
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+  const duration = req?.body?.duration?.length !== 0 ? req.body.duration : undefined;
+  const budget = req?.body?.budget?.length !== 0 ? req.body.budget : undefined;
+  const link = req?.body?.link?.length !== 0 ? req.body.link : undefined;
+
+  console.log('POST /Film');
+
+  if (!title || !duration || !budget || !link) return res.sendStatus(400); // error code '400 Bad request'
+
+  const lastItemIndex = MENU?.length !== 0 ? MENU.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? MENU[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  const newFilm = {
+    id: nextId,
+    title: title,
+    duration: duration,
+    budget: budget,
+    link: link,
+  };
+
+  MENU.push(newFilm);
+
+  res.json(newFilm);
+});
 
 module.exports = router;
